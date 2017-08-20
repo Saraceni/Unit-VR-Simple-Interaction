e1b7ec09d0bd02f4536c2b3f1f1205fb69881677
Saraceni <saraceni.br@gmail.com>
2017-08-20 14:55:06 -0300

    First Commit

---
+++ Assets/Scripts/GlowBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowBehaviour : MonoBehaviour {
+
+	public Material HighlightMaterial; 
+
+	private Material m_NormalMaterial;
+	private Renderer m_Renderer;
+
+	// Use this for initialization
+	void Start () {
+		m_Renderer = GetComponent<Renderer> ();
+		m_NormalMaterial = m_Renderer.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Glow() {
+		m_Renderer.material = HighlightMaterial;
+	}
+
+	public void Normal() {
+		m_Renderer.material = m_NormalMaterial;
+	}
+}
---
+++ Assets/VRStandardAssets/VRInteractiveItem.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace VRStandardAssets {
+	
+	public class VRInteractiveItem : MonoBehaviour {
+
+		public UnityEvent OnOver;             // Called when the gaze moves over this object
+		public UnityEvent OnOut;              // Called when the gaze leaves this object
+		public UnityEvent OnClick;            // Called when click input is detected whilst the gaze is over this object.
+		public UnityEvent OnDoubleClick;      // Called when double click input is detected whilst the gaze is over this object.
+		public UnityEvent OnUp;               // Called when Fire1 is released whilst the gaze is over this object.
+		public UnityEvent OnDown;             // Called when Fire1 is pressed whilst the gaze is over this object.
+
+
+		protected bool m_IsOver;
+
+
+		public bool IsOver
+		{
+			get { return m_IsOver; }              // Is the gaze currently over this object?
+		}
+
+
+		// The below functions are called by the VREyeRaycaster when the appropriate input is detected.
+		// They in turn call the appropriate events should they have subscribers.
+		public void Over()
+		{
+			m_IsOver = true;
+
+			if (OnOver != null)
+				OnOver.Invoke ();
+		}
+
+
+		public void Out()
+		{
+			m_IsOver = false;
+
+			if (OnOut != null)
+				OnOut.Invoke ();
+		}
+
+
+		public void Click()
+		{
+			if (OnClick != null)
+				OnClick.Invoke ();
+		}
+
+
+		public void DoubleClick()
+		{
+			if (OnDoubleClick != null)
+				OnDoubleClick.Invoke ();
+		}
+
+
+		public void Up()
+		{
+			if (OnUp != null)
+				OnUp.Invoke ();
+		}
+
+
+		public void Down()
+		{
+			if (OnDown != null)
+				OnDown.Invoke ();
+		}
+	}
+}
